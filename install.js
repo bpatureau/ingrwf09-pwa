f9221ab297da399457fa20f88b86f534937df31f
Bastien <bastien.patureau@yahoo.com>
2021-08-23 11:55:39 +0200

    ref

--- install.js
+++ install.js
@@ -1,7 +1,16 @@
 const btnInstall = document.querySelector('.install')
 
+let deferredPrompt
+
 
 window.addEventListener("befreinstallprompt", (e) => {
   e.preventDefault()
+  deferredPrompt = e
   btnInstall.classList.remove('hidden')
 } )
+
+btnInstall.addEventListener("click", (e) => {
+  e.preventDefault()
+  btnInstall.classList.add('hidden')
+  deferredPrompt.prompt()
+})
